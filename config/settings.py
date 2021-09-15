96fd5851ffb4d5556da22c30bd07c35b7f4289dc
Cesar Garcia <celord@gmail.com>
2021-09-15 14:38:40 -0600

    Advanced django site start

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #Local
     'accounts',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,8 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #'DIRS': [str(BASE_DIR.joinpath('templates'))], #op1
+        'DIRS': ['templates',BASE_DIR], #op2
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
