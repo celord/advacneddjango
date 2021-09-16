d22a48fa6a225981aff769a0b18b29a2fa4ffb44
Cesar Garcia <celord@gmail.com>
2021-09-16 16:02:32 -0600

    User registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+
 class CustomUserCreationForm(UserCreationForm):
-    model = get_user_model()
-    fields = ('email','username')
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username')
+
 
 class CustomUserChangeForm(UserChangeForm):
-    model = get_user_model()
-    fields = ('email', 'username')
+
+    class Meta:
+
+        model = get_user_model()
+        fields = ('email', 'username')
