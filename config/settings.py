d8b5ada45bbfec5942591864652288f5a1609fec
Cesar Garcia <celord@gmail.com>
2021-09-17 12:02:14 -0600

    bootstrap, static files

--- config/settings.py
+++ config/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    #Third party
+    'crispy_forms',
     #Local
     'accounts',
     'pages'
 ]
+#django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,6 +130,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+#STATICFILES_DIRS = ['static', BASE_DIR]
+
+
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+#STATIC_ROOT = ['staticfiles', BASE_DIR]
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+#STATICFILES_FINDERS = [
+#    "django.contrib.staticfiles.finders.FileSystemFinder",
+#    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+#]
