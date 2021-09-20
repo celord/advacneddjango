3282ddeab1c3c8f4041102556595af7f7d92e069
Cesar Garcia <celord@gmail.com>
2021-09-20 09:46:46 -0600

    File/ image uploads

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # config/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -27,4 +29,5 @@ urlpatterns = [
     # Local apps
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
-]
+    
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
